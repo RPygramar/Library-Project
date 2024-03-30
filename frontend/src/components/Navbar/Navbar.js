d0dd9e376bbab52fbc1bc7fa0f642d9970896055
Erik Daskalyuk <erik.daska@gmail.com>
2024-03-30 17:55:17 +0000

    ~ modificações da nav-bar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,7 +1,21 @@
+import styles from './Navbar.module.css';
+import logo from '../../assets/colares_sintra_logo.png';
+
+
 export default function Navbar(){
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
-    )
+        <>
+      <header className={styles.header}>
+        <img className= {styles.image} src={logo}></img>
+        <nav className={styles.navbar}>
+          <a href="">Home</a>
+          <a href="/livros">Livros</a>
+          <a href="/carrinho">Carrinho</a>
+          <a href="#">Services</a>
+          <a href="#">Contact</a>
+        </nav>
+      </header>
+    </>
+  );
 }
+
