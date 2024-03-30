a571ff61bfa18fa0581db358acee009058532e14
Erik Daskalyuk <erik.daska@gmail.com>
2024-03-30 19:19:23 +0000

    ~ simple ammend 0.0.5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Main_Page from "./Pages/Main_Page/Main_Page";
-import Navbar from "./Components/Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+        <Main_Page></Main_Page>
     </>
   );
 }
