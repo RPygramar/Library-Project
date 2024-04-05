80693b5a5efbd62a1087602a757764ac9ee96c88
Erik Daskalyuk <erik.daska@gmail.com>
2024-04-05 16:39:47 +0100

    ~ Paginations added ~ Page numbers added and styled

--- frontend/src/Pages/BooksPage/BooksPage.js
+++ frontend/src/Pages/BooksPage/BooksPage.js
@@ -1,25 +1,29 @@
 import Navbar from "../../components/Navbar/Navbar";
 import BooksCards from "../../components/BooksCards/BooksCards";
-import {fetchAllBooks} from "../../fetchdata";
+import {fetchAllBooks, fetchTenBooks} from "../../fetchdata";
 import React, { useState, useEffect } from 'react';
 import Styles from './BooksPage.module.css';
 import FilterBar from "../../components/FilterBar/FilterBar";
-
+import ReactPaginate from "react-paginate";
+import { Pagination } from "flowbite-react";
 export default function BooksPage() {
     const [books, setBooks] = useState([]);
     const [authors, setAuthors] = useState([]);
     const [categorys, setCategorys] = useState([]);
     const [isDataLoaded, setIsDataLoaded] = useState(false);
-
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(5);
     const fetchData = async () => {
         try {
-            const fetchedBooks = await fetchAllBooks();
-            setBooks(fetchedBooks);
+            const fetchedBooks = await fetchTenBooks(currentPage);
+            setBooks(fetchedBooks.data);
 
-            const extractedAuthors = fetchedBooks.flatMap((book) => (book.authors ? book.authors : []));
+            const fetchAllBooksData = await fetchAllBooks();
+
+            const extractedAuthors = fetchAllBooksData.flatMap((book) => (book.authors ? book.authors : []));
             const uniqueAuthors = [...new Set(extractedAuthors)];
 
-            const extractedCategorys = fetchedBooks.flatMap((book) => (book.categories ? book.categories : []));
+            const extractedCategorys = fetchAllBooksData.flatMap((book) => (book.categories ? book.categories : []));
             const uniqueCategorys = [...new Set(extractedCategorys)];
 
             setAuthors(uniqueAuthors);
@@ -33,27 +37,51 @@ export default function BooksPage() {
     };
 
     useEffect(() => {
-        fetchData();
-    }, []);
+        fetchData().then(r => console.log('Data fetched', books.data));
+    }, [currentPage]); // Run fetchData whenever the currentPage changes
+
+    const handlePageClick = (data) => {
+        console.log(data.selected)
+        const selectedPage = data.selected + 1;
+        setCurrentPage(selectedPage); // Update currentPage state
+    };
+
+
 
     return (
-        <>
-            {isDataLoaded && ( // Render FilterBar only when data is loaded
+         <>
+            {isDataLoaded && (
                 <FilterBar authors={authors} categorys={categorys} />
             )}
-        <ul className={Styles.booksContainer}>
-        {books && books.length > 0 && books.map((book, index) => (
-          <li key={index} className={Styles.bookItem}>
-            <BooksCards
-              bookURL={book.thumbnailUrl}
-              bookTitle={book.title}
-              bookAuthor={book.authors ? book.authors.join(', ') : 'Unknown Author'}
-              bookPrice={`${book.price} €`}
-            />
-          </li>
-        ))}
-      </ul>
-    </>
+            <ul className={Styles.booksContainer}>
+                {books && books.length > 0 && books.map((book, index) => (
+                    <li key={index} className={Styles.bookItem}>
+                        <BooksCards
+                            bookURL={book.thumbnailUrl}
+                            bookTitle={book.title}
+                            bookAuthor={book.authors ? book.authors.join(', ') : 'Unknown Author'}
+                            bookPrice={`${book.price} €`}
+                        />
+                    </li>
+                ))}
+            </ul>
+            <div>
+                <ReactPaginate
+                    previousLabel={'< Anterior'}
+                    nextLabel={'Próximo >'}
+                    breakLabel={'...'}
+                    pageCount={totalPages}
+                    onPageChange={handlePageClick}
+                    containerClassName={Styles.pagination}
+                    pageLinkClassName={Styles.pageNum}
+                    pageItemClassName={Styles.pageNum}
+                    previousLinkClassName={Styles.pageNum}
+                    nextLinkClassName={Styles.pageNum}
+                    activeClassName={Styles.active}
+                />
+
+            </div>
+        </>
   );
 }
 
--- frontend/src/fetchdata.js
+++ frontend/src/fetchdata.js
@@ -10,6 +10,22 @@ export function fetchAllBooks() {
       console.error("An error occurred: ", error);
     });
 }
+// Anotar que isto ainda está hardcoded, esta a tirar 10 livros apenas porque sim.
+//Com o limit para 10 nao funciona
+export async function fetchTenBooks(page) {
+  try {
+    const response = await fetch(`http://localhost:3030/books?_page=${page}`);
+
+    if (!response.ok) {
+      throw new Error("Error loading data");
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("An error occurred: ", error);
+    throw error; // re-throw the error to be handled by the caller
+  }
+}
 
 export function fetchBookByID(id) {
   return fetch("http://localhost:3030/books/" + id)
